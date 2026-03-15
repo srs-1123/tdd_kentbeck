cbee43642932755af46713db6a2cba7d4bc7e530
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-15 22:25:49 +0900

    テスト実行の責務を親クラスTestCaseに分離

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,15 +1,26 @@
-class WasRun:
+# wasRunがテストの実行状況の保持、テスト実行の２つの責務を持っていたため分離
+class TestCase:
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
     def run(self):
-        # テストケース名を示す属性を問い合わせ、返ってきたオブジェクトを関数のように呼び出す
+        # Pluggable Selectorパターン
+        #   テストケース名を示す属性を問い合わせ
+        #   返ってきたオブジェクトを関数のように呼び出す
         method = getattr(self, self.name)
         method()
+    
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
