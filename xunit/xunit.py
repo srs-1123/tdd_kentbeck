4233ee789ea9bcdfcbc793727b94f5457036351a
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-17 23:37:37 +0900

    setUpメソッドを追加し、WasRunクラスの初期化を修正

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -2,25 +2,32 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
         # Pluggable Selectorパターン
         #   テストケース名を示す属性を問い合わせ
         #   返ってきたオブジェクトを関数のように呼び出す
+        self.setUp()
         method = getattr(self, self.name)
         method()
     
 class WasRun(TestCase):
-    def __init__(self, name):
+    def setUp(self):
         self.wasRun = None
-        super().__init__(name)
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
