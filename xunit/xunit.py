ebe0711fc1a25abed504cb8ae05643eedc7e49b2
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-21 21:26:43 +0900

    TestSuiteを導入

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,26 +1,3 @@
-# wasRunがテストの実行状況の保持、テスト実行の２つの責務を持っていたため分離
-class TestCase:
-    def __init__(self, name):
-        self.name = name
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-    def run(self):
-        # Pluggable Selectorパターン
-        #   テストケース名を示す属性を問い合わせ
-        #   返ってきたオブジェクトを関数のように呼び出す
-        result = TestResult()
-        result.testStarted()
-        self.setUp()
-        try:
-            method = getattr(self, self.name)
-            method()
-        except:
-            result.testFailed()
-        self.tearDown()
-        return result
-
 class TestResult:
     def __init__(self):
         self.runCount = 0
@@ -31,6 +8,36 @@ class TestResult:
         self.errorCount = self.errorCount + 1
     def summary(self):
         return "%d run, %d failed" % (self.runCount, self.errorCount)
+        
+# wasRunがテストの実行状況の保持、テスト実行の２つの責務を持っていたため分離
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def run(self, result):
+        # Pluggable Selectorパターン
+        #   テストケース名を示す属性を問い合わせ
+        #   返ってきたオブジェクトを関数のように呼び出す
+        result.testStarted()
+        self.setUp()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
+        self.tearDown()
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+    def add(self, test):
+        self.tests.append(test)
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
     
 class WasRun(TestCase):
     def setUp(self):
@@ -43,30 +50,42 @@ class WasRun(TestCase):
         self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert("setUp testMethod tearDown " == test.log)
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
     # testFailedResulFormatting でまず失敗結果を記録する機能を実装
     # その後で大きなステップであるこのテストを復活させた
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
     # 例外を出してそれをresultに入れる前に、例外を出さずにresultに記録するテストを追加
     # (小さなステップ)
     def testFailedResulFormatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result) # Collecting Parameterパターン
+        assert("2 run, 1 failed" == self.result.summary())
 
 # summary() を追加したことでテスト結果をprintできるようになった
-print(TestCaseTest("testTemplateMethod").run().summary())
-print(TestCaseTest("testResult").run().summary())
-print(TestCaseTest("testFailedResult").run().summary())
-print(TestCaseTest("testFailedResulFormatting").run().summary())
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResulFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
