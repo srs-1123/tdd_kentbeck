53a34c174d83ce1781d872578f0374f4cf48d120
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-21 21:03:02 +0900

    TestResultクラスを実装

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -10,16 +10,29 @@ class TestCase:
         # Pluggable Selectorパターン
         #   テストケース名を示す属性を問い合わせ
         #   返ってきたオブジェクトを関数のように呼び出す
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
+
+class TestResult:
+    def __init__(self):
+            self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
     
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def testBrokenMethod(self):
+        raise Exception
     def tearDown(self):
         self.log = self.log + "tearDown "
 
@@ -28,5 +41,15 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
